550d9d2324f96b7821fb5cf14442f9b2a0550577
Chagallah <hchagalla@gmail.com>
2022-09-09 14:51:34 -0700

    Alphabet q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Print letters except q and e
+ * Return: 0 om success
+ */
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
