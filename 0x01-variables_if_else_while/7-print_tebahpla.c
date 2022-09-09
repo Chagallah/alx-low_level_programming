fc4ad04b9224cb604e301add7e51641c95dff0aa
Chagallah <hchagalla@gmail.com>
2022-09-09 15:28:41 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Alphabet reverse
+ * Return: 0 success
+ */
+int main(void)
+{
+	char a = 'z';
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
