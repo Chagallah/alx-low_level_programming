af4beb5ee789cb28b4e92a5b778976699b5d213f
Chagallah <hchagalla@gmail.com>
2022-09-10 05:57:56 -0700

    Two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints possible combination of 2, 2-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 100; num1++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			if (num1 < num2)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
