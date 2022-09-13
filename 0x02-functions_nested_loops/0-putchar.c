44ae204bd026d7ccb7d98dabcd043f41c04e6684
Chagallah <hchagalla@gmail.com>
2022-09-13 02:26:05 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Main -Entry
+ * Description - Prints putchar
+ * Reeturn -void.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
