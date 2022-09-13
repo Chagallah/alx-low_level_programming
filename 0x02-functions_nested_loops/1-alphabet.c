d0b84f023ece82e012bc47625283c37009a1b3cf
Chagallah <hchagalla@gmail.com>
2022-09-13 03:11:19 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha ++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
