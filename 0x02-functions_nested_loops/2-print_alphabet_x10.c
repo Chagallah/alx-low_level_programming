a93af1c0ce4355fefe452dff912b60c64767d8a7
Chagallah <hchagalla@gmail.com>
2022-09-14 01:12:11 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the
+ * alphabet in lowercase.
+ *
+ * Return: always 0.
+ */
+
+void print_alphabet_x10(void);
+{
+	char alpha, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		co++;
+		_putchar('\n');
+	}
+	return 0;
+}
