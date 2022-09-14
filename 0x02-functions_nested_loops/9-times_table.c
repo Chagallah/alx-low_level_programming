19c5fabefac5f35e1594f7a74c53f319043c40ce
Chagallah <hchagalla@gmail.com>
2022-09-14 03:04:07 -0700

    Timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table
+ * r = row, c = column, n = digits of result
+ * Return: times table
+ */
+
+void times_table(void)
+{
+	int r, c , d;
+
+	for ( r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++);
+		{
+			n = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
