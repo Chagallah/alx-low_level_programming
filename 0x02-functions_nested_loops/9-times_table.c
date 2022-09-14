16f2ce8451900ca717156bbf8bbe257975646adc
Chagallah <hchagalla@gmail.com>
2022-09-14 03:08:51 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,12 @@ void times_table(void)
 {
 	int r, c , d;
 
-	for ( r = 0; r <= 9; r++)
+	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (c = 1; c <= 9; c++);
+		for (c = 1; c <= 9; c++)
 		{
 			n = (r * c);
 			if ((d / 10) > 0)
