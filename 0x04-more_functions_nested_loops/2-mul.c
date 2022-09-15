cc04ea4ad53c949a2881488f341503f0334aa54f
Chagallah <hchagalla@gmail.com>
2022-09-15 02:40:54 -0700

    Checks for a digit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * mul(int a, int b); - function that multiplies integers
- * @a: int to be multiplied
- * @b: int to be multiplied
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
  * Return: output.
  */
 int mul(int a, int b)
