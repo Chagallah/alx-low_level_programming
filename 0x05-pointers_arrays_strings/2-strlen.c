e7da4afbb5479a650ed6664273c030ce471c092b
Chagallah <hchagalla@gmail.com>
2022-09-19 02:03:56 -0700

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen(char *s) - function that returns the length of a string
+ * _strlen - function that returns the length of a string
  * @s: input string length
  * Return: length of the string
  */
