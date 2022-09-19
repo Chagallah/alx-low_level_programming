ff6cf5789477b8e497719d6243d55b484ad53000
Chagallah <hchagalla@gmail.com>
2022-09-19 01:43:41 -0700

    Function that takes a pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and update
+ * reset_to_98 - function that takes a pointer to an int as parameter and
  * updates the value it points to 98
  * @n: int pointer
  * Return: 0 success
