02c968a387aacbd0f5ace9b902351ea208d15bc3
Chagallah <hchagalla@gmail.com>
2022-09-27 08:09:47 -0700

    Corrections

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(acccept + j) != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 				return (s + i);
