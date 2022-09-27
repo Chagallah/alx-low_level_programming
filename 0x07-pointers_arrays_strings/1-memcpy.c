bf5e15a45ae49a42a5c66ff1a2b10e92b25e51a8
Chagallah <hchagalla@gmail.com>
2022-09-27 07:55:18 -0700

    Changes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,8 +8,10 @@
  * Return: pointer to dest
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		*(dest = i) = (src + i);
 
