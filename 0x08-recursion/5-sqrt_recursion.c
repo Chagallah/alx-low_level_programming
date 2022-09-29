2aa7907ca55cf1402851293cc6ad2e240cfc1e1f
Chagallah <hchagalla@gmail.com>
2022-09-29 10:16:52 -0700

    Square root of natural number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * power_operation - returns natural square root of a number
+ * @n: integer
+ * @c: iterator
+ * Return: square root or -1
+ */
+
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: integer
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n);
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
