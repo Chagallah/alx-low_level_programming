a65c00bc3d922efd0c1177abcd4d7e937cc530f3
Chagallah <hchagalla@gmail.com>
2022-09-29 05:24:02 -0700

    Length of astring

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion(char *s)
+ * _strlen_recursion - returns the lenthg of a string
  * @s: string
  * Return: length of the string
  */
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return  (0);
 	else
-		return (1 + _strlen_recursion(s+ 1));
+		return (1 + _strlen_recursion(s + 1));
 }
