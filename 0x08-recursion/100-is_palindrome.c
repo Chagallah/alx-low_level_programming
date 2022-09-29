242169607e651539edc7fe652897d6b1ffcdff61
Chagallah <hchagalla@gmail.com>
2022-09-29 11:17:47 -0700

    Checks if palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		retur (1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -28,13 +28,14 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
+		return (0 + comparator(s, n1 + 1, n2 -1));
 	}
 	return (0);
 }
 
 /**
  * is_palindrome -detects if a string is a palindrome
- * @S: string
+ * @s: string
  * Return: 1 if string is palindrome and 0 if not
  */
 
