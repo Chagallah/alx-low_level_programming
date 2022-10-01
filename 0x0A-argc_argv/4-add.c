c209b0ab8240f3b85bd9374028a05b012513303d
Chagallah <hchagalla@gmail.com>
2022-10-01 14:59:56 -0700

    Adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 #include <stdlib.h>
-#include <cypte.h>
-
+#include <ctype.h>
 /**
- * main - adds positive numbers
- * @argc: number of command line arguments
- * @argv: array that contains the program command line arguments
- * Return: 0
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
 	int i, j, add = 0;
