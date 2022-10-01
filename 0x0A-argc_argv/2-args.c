f4464c77f1a04b9fed2fbf3ec3edcd9e3393c678
Chagallah <hchagalla@gmail.com>
2022-10-01 14:10:52 -0700

    Prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints all arguments it receives
+ *  main - prints all arguments it receives
  * @argc: number of command line arguments
  * @argv: array that contains the program command line arguments
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int count;
