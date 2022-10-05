5544fa2b03633892fbde3e3796df201b1e3d6860
Chagallah <hchagalla@gmail.com>
2022-10-05 07:48:00 -0700

    Create the static library

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,5 @@
+#Static libraries
+
+- C static libraries
+- Creating a static "C" library using "ar" and "ranlib"
+- Using a "C" library in a program
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ *_putchar - entry point
+ *@c: character to print
+ *return 1
+ *on error -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
